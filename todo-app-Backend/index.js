22c30af23ff0c433af6a23938c6c3ae01db6adab
shaifali <shaifali02000@gmail.com>
2024-04-10 14:40:47 +0530

    last deploy

--- todo-app-Backend/index.js
+++ todo-app-Backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+app.use(cors()); 
 
 const {connection, TodoModel} = require('./db')
 app.use(express.json());
